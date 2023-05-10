765f550388dfaacba0c1578616a70eac44222a90
Elxan Memmedov <elxan5758@gmail.com>
2023-05-10 21:45:45 +0400

    first commit

---
+++ bank/DAL/DataContext/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using bank.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace bank.DAL.DataContext
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+            //test
+            //test2
+            //test3
+        }
+        DbSet<User> Users { get; set; } 
+    }
+}
---
+++ bank/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace bank.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Fin { get; set; }
+        public string UserName { get; set; }  
+        public string Password { get; set; }
+    }
+}
